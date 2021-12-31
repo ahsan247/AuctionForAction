844c332854a53ae51249a2a106744dc781dab1ff
ahsan247 <apurbaahsan@gmail.com>
2022-01-01 03:48:24 +0600

    Create head.php
    Header

---
+++ view/common/head.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Auction For Action</title>
+
+<meta name="description" content="Source code generated using layoutit.com">
+<meta name="author" content="LayoutIt!">
+<style type="text/css">
+    body {
+        overflow-x: hidden;
+    }
+</style>
+<link href="view/css/bootstrap.min.css" rel="stylesheet">
+<link href="view/css/style.css" rel="stylesheet">
+<script src="view/js/jquery.min.js"></script>
+<script src="view/js/bootstrap.min.js"></script>
+<script src="view/js/scripts.js"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
