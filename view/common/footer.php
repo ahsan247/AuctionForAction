c3e67a725c749ad68da08cccd70c54be90689f98
ahsan247 <apurbaahsan@gmail.com>
2022-01-01 03:48:18 +0600

    Create footer.php
    Footer

---
+++ view/common/footer.php
@@ -0,0 +1,22 @@
+<!--
+<div class="container-fluid">
+	<div class="row" id="footer" style="background-image: url('productimages/foot.png'); background-size: cover">
+		<h4 align="center" style="margin-top: 100px; margin-bottom: " "> @Copyright2015 AuXon</h1>
+	</div>
+</div>-->
+
+<div class="d-flex flex-column" style="background: #222222;width: 110%;margin-left: -15px;height: 75px;margin-top: 50px;">
+
+    <!-- FOOTER -->
+    <footer class="w-100 py-4 flex-shrink-0" style="margin-top:100px">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-12 col-md-12">
+                    <p></p>
+                    <h4 class="h4 text-white" style="color:white; text-align: center; font-family:Arial;margin-top:15px"><a class="text-primary" href="https://www.facebook.com/md.shifatul.ahsan.apurba/">Shifatul Ahsan Apurba | CSE470 Project</a></h4>
+                    <p class="small text-muted mb-0" style="color:white; text-align: center">&copy; Copyrights. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
